abdeb9202387caa805e3146f0c49472f46271065
huyetdao431 <142509084+huyetdao431@users.noreply.github.com>
2024-10-20 10:04:11 +0700

    dang nhap + dang ky

---
+++ BTL_LapTrinhWeb/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BTL_LapTrinhWeb.Data;
+using BTL_LapTrinhWeb.ViewModels;
+
+namespace BTL_LapTrinhWeb.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<RegisterVM, KhachHang>();
+                //.ForMember(kh => kh.HoTen, option => option.MapFrom(RegisterVM => RegisterVM.HoTen))
+                //.ReverseMap();
+        }
+    }
+}
