3ac5955aded444b7da7283e74a2cd3db68fb748a
huyetdao431 <142509084+huyetdao431@users.noreply.github.com>
2024-10-24 21:52:03 +0700

    sua sua sua

---
+++ BTL_LapTrinhWeb/ViewModels/BestSalerVM.cs
@@ -0,0 +1,13 @@
+﻿namespace BTL_LapTrinhWeb.ViewModels
+{
+    public class BestSalerVM
+    {
+        public int Mahh { get; set; }
+        public string Tenhh { get; set; }
+        public string? Hinh { get; set; }
+        public double? DonGia { get; set; }
+        public string? MoTaNgan { get; set; }
+        public string TenLoai { get; set; }
+        public int SoLanXem { get; set; }
+    }
+}
